3ddcda050bc50cc55ab468b1aa7f8fb2838d88a6
Thomas Mai <th871752@ucf.edu>
2025-01-21 20:05:20 -0500

    test

--- labs/lab1/Article.js
+++ labs/lab1/Article.js
@@ -4,22 +4,21 @@ import Component from './Component.js';
 // Article is a child object of Component
 class Article extends Component {
 
-  constructor (props) {
+  constructor(props) {
     // Send what it gets to its parent object
+    super(props); // This is required to call the parent class constructor
   }
 
-  render () {
+  render() {
     // Return HTML with the title, author, and text of this article
     // Use template literals for the variables
     return (`
       <Article>
-        <Title>this.props.title</Title>
-        <Author>this.props.author</Author>
-        <Text>this.props.text</Text>
+        <Title>${this.props.title}</Title>
+        <Author>${this.props.author}</Author>
+        <Text>${this.props.text}</Text>
       </Article>
-    `
-    );
-    
+    `);
   }
 
 }
